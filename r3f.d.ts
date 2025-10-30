4ef7d147328a04a240d507e898b0972d1092d69c
jbanmol <jbanmol9@gmail.com>
2025-10-30 21:32:34 +0530

    fix(ts/tsparticles): migrate to @tsparticles v3.0.0, add R3F global types, fix options usage, ensure clean TS build

---
+++ r3f.d.ts
@@ -0,0 +1,8 @@
+import '@react-three/fiber';
+import type { ThreeElements } from '@react-three/fiber';
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements extends ThreeElements {}
+  }
+}
